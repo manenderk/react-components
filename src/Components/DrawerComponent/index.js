ab407241cace253cd7df3e52e87a9d0f9127c51d
Sonu Sharma <sonucs0040@gmail.com>
2021-10-05 16:06:37 +0530

    Drawer Component Added

---
+++ src/Components/DrawerComponent/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+
+const Drawer = () => {
+
+  return (
+    <>
+      <div className="offcanvas offcanvas-end" style={{position:"fixed",right:"38px"}} id="demo">
+        <div className="offcanvas-header">
+          <h1 className="offcanvas-title">Bookmarks</h1>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div className="offcanvas-body">
+          <p>Some text lorem ipsum.</p>
+          <p>Some text lorem ipsum.</p>
+          <button className="btn btn-secondary" type="button">A Button</button>
+        </div>
+      </div> 
+      <div className="offcanvas offcanvas-end" style={{ position: "fixed", right: "38px" }} id="demoTwo">
+        <div className="offcanvas-header">
+          <h1 className="offcanvas-title">Notes</h1>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div className="offcanvas-body">
+          <p>Some text lorem ipsum.</p>
+          <p>Some text lorem ipsum.</p>
+          <button className="btn btn-secondary" type="button">A Button</button>
+        </div>
+      </div>
+      <div className="offcanvas offcanvas-end" style={{ position: "fixed", right: "38px" }} id="demoThree">
+        <div className="offcanvas-header">
+          <h1 className="offcanvas-title">Additional Notes</h1>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div className="offcanvas-body">
+          <p>Some text lorem ipsum.</p>
+          <p>Some text lorem ipsum.</p>
+          <p>Some text lorem ipsum.</p>
+          <button className="btn btn-secondary" type="button">A Button</button>
+        </div>
+      </div>
+      <div className="d-flex gap-2" style={{ position: "fixed", top: "30vh", right: "-150px", zIndex: 9999, transform: "rotate(90deg)" }}>
+        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">Bookmarks</button>
+        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demoTwo">Notes</button>
+        <button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demoThree">Additional Notes</button>
+      </div>
+
+    </>
+  )
+}
+
+export default Drawer
+
